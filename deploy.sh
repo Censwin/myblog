bb6e54da719b47f1856289caae67d22e0f53648d
kyle <704334497@qq.com>
2021-07-02 13:55:22 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,4 @@ cd docs/.vuepress/dist
 git add -A
 git commit -m 'deploy'
 
-# 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
-# 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f origin  master:master
-
-cd -
+cd ..
